1ba81d81e2beb3132b3ef48ddc736c6d40f56826
Luiz <lhbertolucci@gmail.com>
2024-03-13 10:56:25 -0300

    # Changes made to the model structure: 1. Removed certain non-essential fields from models to simplify the structure in ConsumerGroup. 2. Introduced "ConsumerGroupUsers" model to manage user associations with ConsumerGroup, including designating admin users. 3. Eliminated the ManyToMany relationship from ConsumerGroup to User. This avoids the need for an additional join table, given that "ConsumerGroupUsers" now serves to organize admin and regular users. 4. It's important to ensure that each ConsumerGroup has at least one user, who must be an admin, as managed by the "ConsumerGroupUsers" model. 5. Added other fields in UserConsumerInvitation

--- server/groups/models.py
+++ server/groups/models.py
@@ -1,29 +1,67 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class ConsumerGroup(models.Model):
     """
-    A consumer group is a group of users that want to buy items.
-    They can participate in any number of communities.
+    Represents a Consumer Group, which can be an individual user or a group of users interested in purchasing items. 
+    This entity is capable of participating in various communities. Each Consumer Group is characterized by 
+    personal and contact information, along with a unique logo and a descriptive text.
     """
+    nickname = models.CharField(max_length=100)  # Nickname of the Consumer Group
+    phone_number = models.CharField(max_length=15)  # Contact phone number
+    postal_code = models.CharField(max_length=10)  # Postal code for the location
+    street_name = models.CharField(max_length=255)  # Name of the street
+    house_number = models.CharField(max_length=10)  # House or business number, accepting alphanumeric values
+    neighborhood = models.CharField(max_length=100)  # Name of the neighborhood
+    reference_location = models.TextField(blank=True, null=True)  # Additional reference location, optional
+    logo = models.ImageField(upload_to='consumer_logos/')  # Logo image, requires MEDIA_ROOT and MEDIA_URL configurations
+    description = models.TextField()  # Descriptive text about the Consumer Group
 
-    nickname = models.CharField(max_length=100)  # String for nickname
-    users = models.ManyToManyField(User)
-    users_admin = models.ManyToManyField(User)
-    document_number = models.CharField(max_length=20)  # String for document number, assuming it's something like a social security number or a business identification number
-    phone_number = models.CharField(max_length=15)  # String for phone number
-    postal_code = models.CharField(max_length=10)  # String for postal code
-    street_name = models.CharField(max_length=255)  # String for street name
-    number = models.CharField(max_length=10)  # String for house/business number, could be an IntegerField, but we use CharField to accept numbers like '12A'
-    neighborhood = models.CharField(max_length=100)  # String for neighborhood
-    reference_location = models.TextField(blank=True, null=True)  # Optional text for a reference location
-    gps_location = models.CharField(max_length=255)  # String for GPS location, could be formatted as 'latitude, longitude'
-    logo = models.ImageField(upload_to='consumer_logos/')  # Image for the logo, MEDIA_ROOT and MEDIA_URL need to be configured in settings.py to work properly
-    presentation = models.TextField()  # Text field for presentation
+    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp for when the record was created
+    updated_at = models.DateTimeField(auto_now=True)  # Timestamp for the last update of the record
 
     def __str__(self):
         return self.nickname
+
+class ConsumerGroupUsers(models.Model):
+    """
+    Defines the association between users and their respective Consumer Group, including a flag to indicate 
+    if a user holds administrative privileges within the group. This model helps in organizing the roles and 
+    memberships of users within Consumer Unities.
+    """
+    consumer_group = models.ForeignKey(ConsumerGroup, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_admin = models.BooleanField()  # Indicates whether the user is an admin of the Consumer Group
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["consumer_group", "user"],  # Corrected field name from "consumer_group" to "consumer_group"
+                name="unique_consumer_group_user",
+            )
+        ]
+
+class UserConsumerInvitation(models.Model):
+    """
+    Manages the invitations sent to users to join specific Consumer Unities. This model ensures each invitation is
+    unique to a user-consumer group pair, preventing duplicate invitations. It tracks the invitation status and
+    allows for a personalized message.
+    """
+    consumer_group = models.ForeignKey(ConsumerGroup, on_delete=models.CASCADE)
+    inviting_user = models.ForeignKey(User, related_name='sent_invitations', on_delete=models.CASCADE)
+    invited_user = models.ForeignKey(User, related_name='received_invitations', on_delete=models.CASCADE)
+    message = models.TextField(blank=True, null=True)  # Optional personalized message
+    status = models.CharField(max_length=10, choices=[('sent', 'Sent'), ('accepted', 'Accepted'), ('declined', 'Declined')], default='sent')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["consumer_group", "inviting_user", "invited_user"],
+                name="unique_user_consumer_invitation",
+            )
+        ]
+
 
 class ProductionType(models.Model):
     name = models.CharField(max_length=20, choices=[
@@ -106,9 +144,6 @@ class Community(models.Model):
         return self.trade_name
 
    
-class UserConsumerInvitations(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    group = models.ForeignKey(ConsumerGroup, on_delete=models.CASCADE)
 
 
 class UserProducerInvitations(models.Model):
