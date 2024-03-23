be8ddb9591dd13a6705f040927118df28d145ea8
Luiz <lhbertolucci@gmail.com>
2024-03-23 12:10:36 -0300

    fixing argument type for signals functions

--- server/users/signals.py
+++ server/users/signals.py
@@ -1,15 +1,13 @@
 from typing import Any, Type
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from django.db.models.base import ModelBase
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from users.models import UserProfile
 
-
 @receiver(post_save, sender=User)
 def create_user_profile_from_user(
-    sender: Type[ModelBase], instance: User, created: bool, **kwargs: Any
+    sender: Type[User], instance: User, created: bool, **kwargs: Any
 ) -> None:
     """
     Automatically creates a UserProfile for a new User instance. If there is an issue
@@ -22,9 +20,8 @@ def create_user_profile_from_user(
             instance.delete()  # Revert User creation in case of an error
             raise e
 
-
 @receiver(pre_save, sender=User)
-def check_email(sender: Type[ModelBase], instance: User, **kwargs: Any) -> None:
+def check_email(sender: Type[User], instance: User, **kwargs: Any) -> None:
     email = instance.email
     if sender.objects.filter(email=email).exclude(username=instance.username).exists():
         raise ValidationError("Email Already Exists")
